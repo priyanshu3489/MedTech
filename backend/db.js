cd1a9b2b4962c5a78e89b491d2bc2ed90acb6391
PRIYANSHU HIRPARA <priyanshuhirpara2111@gmail.com>
2023-08-08 20:23:32 +0530

    8-aug

---
+++ backend/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&directConnection=true&ssl=false"
+
+const connectToMongo = async () => {
+try {
+    mongoose.set('strictQuery', false)
+    mongoose.connect(mongoURI) 
+    console.log('Connected to Mongo Successfully')
+}
+catch(error) {
+    console.log(error)
+    process.exit()
+}
+}
+module.exports = connectToMongo;
