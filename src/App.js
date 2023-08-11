61f6a23403a939ec7f9ea5dbadd19e0b104f8263
PRIYANSHU HIRPARA <priyanshuhirpara2111@gmail.com>
2023-08-11 20:54:44 +0530

    11-aug

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
+import Navbar from './component/Navbar';
+import Footer from './component/Footer';
+import Login from './component/Login';
+import Signup from './component/Signup';
+import About from './component/About';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path='/'>
+              <About />
+            </Route>
+            <Route exact path='/signup'>
+              <Signup />
+            </Route>
+            <Route exact path='/login'>
+              <Login />
+            </Route>
+          </Switch>
+        </div>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
